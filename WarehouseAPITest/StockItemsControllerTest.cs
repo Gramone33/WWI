70106f95466ec347ed62be2dcc2e926485da7e15
S.Labasse <labasse@gmail.com>
2020-04-29 17:20:27 +0200

    Add test to controller with error and extrem test case

--- WarehouseAPITest/StockItemsControllerTest.cs
+++ WarehouseAPITest/StockItemsControllerTest.cs
@@ -11,9 +11,9 @@ namespace WarehouseAPITest
     public class Tests
     {
         private readonly StockItemDto[] predefinedStockItems = new StockItemDto[] {
-            new StockItemDto(){ StockItemID=1, SupplierID=1, StockItemName="A", ColorID=1, ColorName="C1", Barcode="A1", Brand="AA" },
-            new StockItemDto(){ StockItemID=2, SupplierID=1, StockItemName="B", ColorID=3, ColorName="C3", Barcode="B2", Brand="AA" },
-            new StockItemDto(){ StockItemID=3, SupplierID=1, StockItemName="C", ColorID=2, ColorName="C2", Barcode="C3", Brand="BB" }
+            new StockItemDto(){ StockItemID=1, SupplierID=125, StockItemName="A", ColorID=1, ColorName="C1", Barcode="A1", Brand="AA" },
+            new StockItemDto(){ StockItemID=2, SupplierID=125, StockItemName="B", ColorID=3, ColorName="C3", Barcode="B2", Brand="AA" },
+            new StockItemDto(){ StockItemID=3, SupplierID=125, StockItemName="C", ColorID=2, ColorName="C2", Barcode="C3", Brand="BB" }
         };
 
         [Test]
@@ -39,6 +39,51 @@ namespace WarehouseAPITest
             // Assert
             CollectionAssert.AreEqual(predefinedStockItems, actual.Value.StockItems);
             Assert.AreEqual("https://api.example.com/suppliers/125/stockitems?pageToken=2&pageSize=3", actual.Value.NextPage);
+        }
+
+        [Test]
+        public void ListStockItems_LastPage()
+        {
+            // Arrange
+            var mockLogger = new Mock<ILogger<StockItemsController>>();
+            var mockRepo = new Mock<IStockRepository>();
+
+            mockRepo.Setup(repo => repo.ListStockItems(125, 1, 4)).Returns(predefinedStockItems);
+
+            var test = new StockItemsController(mockLogger.Object, mockRepo.Object)
+            {
+                ControllerContext = new ControllerContext()
+                {
+                    HttpContext = new DefaultHttpContext()
+                }
+            };
+            test.HttpContext.Request.Scheme = "https";
+            test.HttpContext.Request.Host = new HostString("api.example.com");
+
+            // Act
+            var actual = test.ListStockItems(125, new ListStockRequest() { PageSize = 4, PageToken = 1 });
+
+            // Assert
+            CollectionAssert.AreEqual(predefinedStockItems, actual.Value.StockItems);
+            //Assert.AreEqual(null, actual.Value.NextPage);
+            Assert.IsNull(actual.Value.NextPage);
+        }
+
+        [Test]
+        public void ListStockItems_InvalidPageToken()
+        {
+            // Arrange
+            var mockLogger = new Mock<ILogger<StockItemsController>>();
+            var mockRepo = new Mock<IStockRepository>();
+            var test = new StockItemsController(mockLogger.Object, mockRepo.Object);
+
+            // Act
+            var actual = test.ListStockItems(125, new ListStockRequest() { PageSize = 4, PageToken = -1 });
+            var result = actual.Result as ObjectResult;
+            
+            // Assert
+            Assert.AreEqual(StatusCodes.Status400BadRequest, result.StatusCode);
+            // ou Assert.IsInstanceOf<BadRequestObjectResult>(actual.Result);
         }
     }
 }
